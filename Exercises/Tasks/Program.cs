29ee665d8eadfebabb5e638cdbb5e65d7c9b0b67
smasher <iluha_m_74@mail.ru>
2023-02-18 11:36:13 +0500

    Added Exercises folder for exercises in all modules

---
+++ Exercises/Tasks/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        static T? Max<T>(T[] source)
+            where T : IComparable<T>
+        {
+            if(source.Length == 0)
+                return default;
+            
+            var max = source[0];
+            for (var i = 1; i < source.Length; i++) 
+                if (source[i].CompareTo(max) > 0)
+                    max = source[i];
+
+            return max;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Max(new int[0]));
+            Console.WriteLine(Max(new[] { 3 }));
+            Console.WriteLine(Max(new[] { 3, 1, 2 }));
+            Console.WriteLine(Max(new[] { "A", "B", "C" }));
+        }
+    }
+}
