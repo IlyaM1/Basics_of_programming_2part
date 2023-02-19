55753b8ff5939285cafc7c129e8d4dbc773ada41
smasher <iluha_m_74@mail.ru>
2023-02-19 18:24:08 +0500

    Made LimitedSizeStack practice

---
+++ LimitedSizeStack/LimitedSizeStack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace LimitedSizeStack;
+
+public class StackItem<T>
+{
+    public T Value { get; set; }
+    public StackItem<T> Next { get; set; }
+    public StackItem<T> Previous { get; set; }
+}
+
+public class LimitedSizeStack<T>
+{
+    public int ElementsLimit { get; private set; }
+    public bool IsEmpty { get { return Count == 0; } }
+    private int _length;
+
+    StackItem<T> _top;
+    StackItem<T> _bottom;
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        ElementsLimit = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        if (ElementsLimit == 0)
+            return;
+
+        if (IsEmpty || ElementsLimit == 1)
+        {
+            _bottom = _top = new StackItem<T> { Value = item, Next = null, Previous = null };
+            Count = 1;
+        }
+        else
+        {
+            if (Count == ElementsLimit)
+            {
+                _bottom = _bottom.Next;
+                _bottom.Previous = null;
+            }
+            else
+                Count += 1;
+
+            _top = new StackItem<T> { Value = item, Next = null, Previous = _top };
+            _top.Previous.Next = _top;
+        }
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty)
+            throw new InvalidOperationException();
+
+        var result = _top.Value;
+        if (Count == 1)
+            _top = _bottom = null;
+        else
+            _top = _top.Previous;
+
+        Count -= 1;
+        return result;
+    }
+
+    public int Count
+    {
+        get { return _length; }
+        set
+        {
+            if (value < 0) throw new ArgumentException();
+            _length = value;
+        }
+    }
+}
---
+++ LimitedSizeStack/ListModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class ListModel<TItem>
+{
+	public List<TItem> Items { get; }
+	public int UndoLimit;
+	public LimitedSizeStack<TItem> UndoActionsStack { get; set; }
+	public LimitedSizeStack<int> RemovedItemsIndexes { get; set; }
+        
+	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+	{
+	}
+
+	public ListModel(List<TItem> items, int undoLimit)
+	{
+		Items = items;
+		UndoLimit = undoLimit;
+		UndoActionsStack = new LimitedSizeStack<TItem>(undoLimit);
+        RemovedItemsIndexes = new LimitedSizeStack<int>(undoLimit);
+    }
+
+	public void AddItem(TItem item)
+	{
+        UndoActionsStack.Push(item);
+        Items.Add(item);
+	}
+
+	public void RemoveItem(int index)
+	{
+        UndoActionsStack.Push(Items[index]);
+        Items.RemoveAt(index);
+		RemovedItemsIndexes.Push(index);
+    }
+
+	public bool CanUndo()
+	{
+		return UndoActionsStack.Count > 0;
+	}
+
+	public void Undo()
+	{
+		var lastActionItem = UndoActionsStack.Pop();
+		if (Items.Contains(lastActionItem))
+            Items.RemoveAt(Items.IndexOf(lastActionItem));
+		else
+            Items.Insert(RemovedItemsIndexes.Pop(), lastActionItem);
+	}
+}
