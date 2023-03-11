a4f59b3d0069cc7b53b346e12c657a86392f5b06
smasher <iluha_m_74@mail.ru>
2023-03-12 02:25:18 +0500

    Trying to do 3rd practice

--- brainfuck/BrainfuckLoopCommands.cs
+++ brainfuck/BrainfuckLoopCommands.cs
@@ -1,13 +1,69 @@
+using System;
 using System.Collections.Generic;
 
 namespace func.brainfuck
 {
 	public class BrainfuckLoopCommands
 	{
-		public static void RegisterTo(IVirtualMachine vm)
+        public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
-		}
+			FindLeftAndRightBrackets(vm);
+
+            vm.RegisterCommand('[', b => {
+				if (vm.Memory[vm.MemoryPointer] == 0)
+				{
+                    var tuple = FindLeftAndRightBrackets(vm);
+                    var left = tuple.Item1;
+                    var right = tuple.Item2;
+                    vm.InstructionPointer = right[left.IndexOf(vm.InstructionPointer)];
+                }
+					
+			});
+            vm.RegisterCommand(']', b => {
+                if (vm.Memory[vm.MemoryPointer] != 0)
+				{
+                    var tuple = FindLeftAndRightBrackets(vm);
+                    var left = tuple.Item1;
+                    var right = tuple.Item2;
+                    var IP = vm.InstructionPointer;
+					var index = right.IndexOf(IP);
+					var leftBracket = left[index];
+                    vm.InstructionPointer = leftBracket;
+                } 
+                    
+            });
+        }
+
+		private static Tuple<List<int>, List<int>> FindLeftAndRightBrackets(IVirtualMachine vm)
+		{
+            var _leftBrackets = new List<int>();
+            var _rightBrackets = new List<int>();
+
+			//var commands = vm.Instructions.Replace('\r', ' ').Replace('\n', ' ').Replace(" ", "");
+			var commands = vm.Instructions;
+
+            var left = 0;
+			var right = commands.Length - 1;
+
+			while (left < right)
+			{
+				if (commands[left] == '[')
+				{
+					_leftBrackets.Add(left);
+					while (left < right)
+					{
+						if (commands[right] == ']')
+						{
+							_rightBrackets.Add(right);
+							break;
+						}
+						right--;
+					}
+				}
+				left++;
+			}
+
+			return new Tuple<List<int>, List<int>>(_leftBrackets, _rightBrackets);
+        }
 	}
 }
